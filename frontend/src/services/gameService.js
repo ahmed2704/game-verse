26f57e85022bcdb0aaac5ddc95aa6d84e9dc3ea6
ahmed2704 <kassimahmed2704@gmail.com>
2024-09-17 09:18:09 -0400

    gameService.js complete

--- frontend/src/services/gameService.js
+++ frontend/src/services/gameService.js
@@ -10,3 +10,23 @@ export function search(query) {
 console.log(query);
   return sendRequest(`${BASE_URL}/search/?search=${query}`, 'GET');
 }
+
+export function rawGShow(id) {
+  return sendRequest(`${BASE_URL}/rawGShow/${id}`, 'GET');
+}
+
+export function showLikedGames() {
+  return sendRequest(`${BASE_URL}/showLikedGames`, 'GET');
+}
+
+export function createReview(gameId, reviewData) {
+  return sendRequest(`${BASE_URL}/${gameId}/reviews`, 'POST', reviewData);
+}
+
+export function updateReview(gameId, reviewId, reviewData) {
+  return sendRequest(`${BASE_URL}/${gameId}/reviews/${reviewId}`, 'PUT', reviewData);
+}
+
+export function deleteReview(gameId, reviewId) {
+  return sendRequest(`${BASE_URL}/${gameId}/reviews/${reviewId}`, 'DELETE');
+}
